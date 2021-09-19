22cb45ee28eaa04f99f83165b952ce908b32bb8e
darkosever <darkosevrof@gmail.com>
2021-09-19 21:46:48 +0200

    ig

---
+++ instagram_api/igapi.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ig API mf 1</title>
+</head>
+<body>
+    <div>
+    <h1>This is <?php echo "Instagram API"; ?></h1>
+    </div>
+    <style>
+        body {
+            font-family:arial;
+            color:white;
+            background-color:lightgray;
+        }
+        div {
+
+        }
+    </style>
+</body>
+</html>
